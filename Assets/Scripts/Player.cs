67589dc27cb171d754d1ec5a9fbd680522391848
mw2358006 <2358006@mokwon.ac.kr>
2025-01-07 11:31:17 +0900

    Day 3

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,11 +3,16 @@ public class Player : MonoBehaviour
 {
   public float speed = 0f;
   Vector2 inputVec = Vector2.zero;
+
   Rigidbody2D rigid = null;
+  SpriteRenderer spriter = null;
+  Animator anim = null;
 
   void Awake()
   {
     rigid = GetComponent<Rigidbody2D>();
+    spriter = GetComponent<SpriteRenderer>();
+    anim = GetComponent<Animator>();
   }
 
   void Update()
@@ -20,5 +25,15 @@ public class Player : MonoBehaviour
   {
     Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;
     rigid.MovePosition(rigid.position + nextVec);
+  }
+
+  void LateUpdate()
+  {
+    anim.SetFloat("Speed", inputVec.magnitude);
+
+    if (inputVec.x != 0)
+    {
+      spriter.flipX = inputVec.x < 0;
+    }
   }
 }
